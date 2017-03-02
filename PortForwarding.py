e7730a7d9ffeac1ebc78f10890fdcb3a3b65a1b7
Akash Darak <akash.darak@colostate.edu>
2017-03-01 22:32:59 -0700

    Update PortForwarding.py

--- PortForwarding.py
+++ PortForwarding.py
@@ -1,28 +1,5 @@
 #!/usr/bin/python
 
-#----------------------------------------------------------------------
-# Copyright (c) 2013 Raytheon BBN Technologies
-#
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and/or hardware specification (the "Work") to
-# deal in the Work without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense,
-# and/or sell copies of the Work, and to permit persons to whom the Work
-# is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Work.
-#
-# THE WORK IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE WORK OR THE USE OR OTHER DEALINGS
-# IN THE WORK.
-#----------------------------------------------------------------------
-
 from pox.core import core
 from pox.openflow import *
 import string
@@ -109,5 +86,4 @@ def launch(config_file=os.path.join(SCRIPT_PATH, "port_forward.config")):
     log.debug("PortForwarding " + config_file);
     config = readConfigFile(config_file, log)
     core.registerNew(PortForwarding, config["general"])
-
 
